8477f6fd82a41d0980ca328ebff3d2c5b546df7a
tcc <tongchuanchuan@yeah.net>
2020-04-19 22:16:26 +0800

    新增我的线索页面相关

---
+++ pages/subordinate/index.vue
@@ -0,0 +1,76 @@
+<template>
+	<view>
+		<cu-custom bgColor="bg-gradual-blue">
+			<block slot="content">查看下线</block>
+		</cu-custom>
+		
+		<view class="box">
+			<view class="cu-bar bg-white">
+				<view class="action">
+					<text class="cuIcon-titles text-green"></text>
+					<text class="text-xl text-bold">我的战绩</text>
+				</view>
+			</view>
+			
+		</view>
+		
+		<view class="cu-card case">
+			<view class="cu-item shadow">
+				<view class="cu-list menu-avatar">
+					<view class="cu-item">
+						<view class="cu-avatar round lg" style="background-image:url(https://ossweb-img.qq.com/images/lol/web201310/skin/big10006.jpg);"></view>
+						<view class="content flex-sub">
+							<view class="text-grey">正义天使 凯尔</view>
+							<view class="text-gray text-sm flex justify-between">
+								销售经理
+								<view class="text-gray text-sm">
+									<text class="margin-lr-xs"></text> 2020-04-19 22:00:00
+								</view>
+							</view>
+						</view>
+					</view>
+					<view class="cu-item">
+						<view class="cu-avatar round lg" style="background-image:url(https://ossweb-img.qq.com/images/lol/web201310/skin/big10006.jpg);"></view>
+						<view class="content flex-sub">
+							<view class="text-grey">正义天使 凯尔</view>
+							<view class="text-gray text-sm flex justify-between">
+								销售经理
+								<view class="text-gray text-sm">
+									<text class="margin-lr-xs"></text> 2020-04-19 22:00:00
+								</view>
+							</view>
+						</view>
+					</view>
+					<view class="cu-item">
+						<view class="cu-avatar round lg" style="background-image:url(https://ossweb-img.qq.com/images/lol/web201310/skin/big10006.jpg);"></view>
+						<view class="content flex-sub">
+							<view class="text-grey">正义天使 凯尔</view>
+							<view class="text-gray text-sm flex justify-between">
+								销售经理
+								<view class="text-gray text-sm">
+									<text class="margin-lr-xs"></text> 2020-04-19 22:00:00
+								</view>
+							</view>
+						</view>
+					</view>
+				</view>
+			</view>
+		</view>
+		
+
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ router/modules/subordinate.js
@@ -0,0 +1,9 @@
+const Subordinate = [{
+	path: '/pages/subordinate/index',
+	name: 'subordinate',
+	aliasPath: '/subordinate',
+	meta: {
+		title: '查看下线',
+	}
+}]
+export default Subordinate
